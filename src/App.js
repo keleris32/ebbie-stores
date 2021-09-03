6ba7a652c2e5771fce2ca52e7539e7d3cc0858e4
keleris32 <charles.ejiegbu@gmail.com>
2021-09-03 14:47:55 +0100

    feat: added a helper function to scroll window to top on page navigation

--- src/App.js
+++ src/App.js
@@ -4,11 +4,13 @@ import { NewNav, Footer } from './Components';
 import Home from './Pages/Home/Home';
 import Apparel from './Pages/Apparel/Apparel';
 import GlobalProvider from './Context/Provider';
+import ScrollToTop from './Helpers/ScrollToTop';
 
 function App() {
   return (
     <GlobalProvider>
       <Router>
+        <ScrollToTop />
         <div className="App">
           <NewNav />
           <Switch>
